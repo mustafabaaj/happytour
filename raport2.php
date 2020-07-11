f7a1f548706e1f411c9f1b2102df2e5fa0e3aecf
zahernasta <zaher.nasta18@gmail.com>
2020-07-11 13:03:11 +0300

    Added the ability to export csv only on certain data in table

--- raport2.php
+++ raport2.php
@@ -57,7 +57,7 @@ require_once './partials/sideBar.php';
         </form>
         </div>
         <div class="row">
-            <table class="table table-striped table-hover">
+            <table class="table table-striped table-hover" id="tableRaport">
                 <thead>
                     <tr>
                         <th>Id</th>
@@ -120,10 +120,40 @@ require_once './partials/sideBar.php';
         </div>
         </tbody>
     </table>
-    <form class="form-horizontal style-form" action="submitForm.php" method="post">
-            <input type="submit" name="exportAgent" value="CSV Export" class="btn btn-success"/>
-    </form>
+    <input type="submit" name="exportAgent" value="CSV Export" onclick="download_table_as_csv('tableRaport')" class="btn btn-success"/>
     </div>
 </body>
 </html>
 <?php require_once './partials/footer.php' ?>
+
+<script>
+    function download_table_as_csv(table_id) {
+    // Select rows from table_id
+    var rows = document.querySelectorAll('table#' + table_id + ' tr');
+    // Construct csv
+    var csv = [];
+    for (var i = 0; i < rows.length; i++) {
+        var row = [], cols = rows[i].querySelectorAll('td, th');
+        for (var j = 0; j < cols.length; j++) {
+            // Clean innertext to remove multiple spaces and jumpline (break csv)
+            var data = cols[j].innerText.replace(/(\r\n|\n|\r)/gm, '').replace(/(\s\s)/gm, ' ')
+            // Escape double-quote with double-double-quote (see https://stackoverflow.com/questions/17808511/properly-escape-a-double-quote-in-csv)
+            data = data.replace(/"/g, '""');
+            // Push escaped string
+            row.push('"' + data + '"');
+        }
+        csv.push(row.join(';'));
+    }
+    var csv_string = csv.join('\n');
+    // Download it
+    var filename = 'export_' + table_id + '_' + new Date().toLocaleDateString() + '.csv';
+    var link = document.createElement('a');
+    link.style.display = 'none';
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv_string));
+    link.setAttribute('download', filename);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+</script>
